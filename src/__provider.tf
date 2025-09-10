21b13474a1af618e549a8ebeb213e93210ed628e
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-09-10 20:24:26 -0300

    feat: adicionar provider do vercel

--- src/__provider.tf
+++ src/__provider.tf
@@ -4,9 +4,17 @@ terraform {
       version = "~> 1.7.5"
       source  = "render-oss/render"
     }
+    vercel = {
+      version = "~> 3.15.1"
+      source  = "vercel/vercel"
+    }
   }
 }
 
 provider "render" {
   api_key = var.render_api_key
+}
+
+provider "vercel" {
+  api_token = var.vercel_api_key
 }
--- src/variables.tf
+++ src/variables.tf
@@ -2,3 +2,8 @@ variable "render_api_key" {
   type      = string
   sensitive = true
 }
+
+variable "vercel_api_key" {
+  type      = string
+  sensitive = true
+}
